b1cd6fccc8a77b7c2af45c2e9fa5ffc685f5d836
Wojciech <rola.wojciech@gmail.com>
2025-12-10 15:53:36 +0100

    feat: traceing witgh outbox pattern

---
+++ 19-tracing/04-tracing-with-outbox/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	watermillSQL "github.com/ThreeDotsLabs/watermill-sql/v3/pkg/sql"
+	"github.com/ThreeDotsLabs/watermill/components/forwarder"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+)
+
+var outboxTopic = "events_to_forward"
+
+func PublishInTx(
+	msg *message.Message,
+	tx *sql.Tx,
+	logger watermill.LoggerAdapter,
+) error {
+	var publisher message.Publisher
+	var err error
+
+	// this publisher publishes "enveloped" message,
+
+	publisher, err = watermillSQL.NewPublisher(
+		tx,
+		watermillSQL.PublisherConfig{
+			SchemaAdapter: watermillSQL.DefaultPostgreSQLSchema{},
+		},
+		logger,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create outbox publisher: %w", err)
+	}
+
+	// it's worth decorating as well to see forwarding operation in trace
+	// (forwarding is done based on enveloped message)
+	publisher = newTracePublisher(publisher)
+
+	// this publisher publishes "our" message
+	publisher = forwarder.NewPublisher(publisher, forwarder.PublisherConfig{
+		ForwarderTopic: outboxTopic,
+	})
+
+	// you should decorate your publisher here
+	publisher = newTracePublisher(publisher)
+
+	return publisher.Publish("ItemAddedToCart", msg)
+}
+
+func RunForwarder(
+	db *sqlx.DB,
+	rdb *redis.Client,
+	outboxTopic string,
+	logger watermill.LoggerAdapter,
+) error {
+	postgresSub, err := watermillSQL.NewSubscriber(
+		db,
+		watermillSQL.SubscriberConfig{
+			SchemaAdapter:  watermillSQL.DefaultPostgreSQLSchema{},
+			OffsetsAdapter: watermillSQL.DefaultPostgreSQLOffsetsAdapter{},
+		},
+		logger,
+	)
+	if err != nil {
+		return err
+	}
+
+	err = postgresSub.SubscribeInitialize(outboxTopic)
+	if err != nil {
+		return err
+	}
+
+	redisPub, err := redisstream.NewPublisher(redisstream.PublisherConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		return err
+	}
+
+	fwd, err := forwarder.NewForwarder(
+		postgresSub,
+		redisPub,
+		logger,
+		forwarder.Config{
+			ForwarderTopic: outboxTopic,
+			Middlewares: []message.HandlerMiddleware{
+				func(h message.HandlerFunc) message.HandlerFunc {
+					return func(msg *message.Message) ([]*message.Message, error) {
+						fmt.Println("Forwarding message", msg.UUID, string(msg.Payload), msg.Metadata)
+
+						return h(msg)
+					}
+				},
+			},
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		err := fwd.Run(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	<-fwd.Running()
+
+	return nil
+}
+
+type TracedPublisher struct {
+	wrapped message.Publisher
+}
+
+func (p *TracedPublisher) Publish(topic string, messages ...*message.Message) error {
+	for i := range messages {
+		otel.GetTextMapPropagator().Inject(messages[i].Context(), propagation.MapCarrier(messages[i].Metadata))
+	}
+	return p.wrapped.Publish(topic, messages...)
+}
+
+func (p *TracedPublisher) Close() error {
+	return p.wrapped.Close()
+}
+
+func newTracePublisher(pub message.Publisher) message.Publisher {
+	return &TracedPublisher{wrapped: pub}
+}
